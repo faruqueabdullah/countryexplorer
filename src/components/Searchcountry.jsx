25288fd9bf65fc63791a48b59ccb02830c4e2e4a
Faruque Abdullah <abdullahfaruque007@gmail.com>
2026-04-16 11:19:53 +0530

    implement debounce to searchcountry component

--- src/components/Searchcountry.jsx
+++ src/components/Searchcountry.jsx
@@ -1,4 +1,18 @@
+import { useEffect, useState } from "react";
+
 export default function Searchcountry({ setQuery }) {
+  const [input, setInput] = useState("");
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setQuery(input);
+    }, 500);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [input]);
+
   return (
     <div className="search-container shadow-md w-full md:w-120 rounded px-6 overflow-hidden flex items-center">
       <i className="fa-solid fa-magnifying-glass"></i>
@@ -6,7 +20,8 @@ export default function Searchcountry({ setQuery }) {
         className=" outline-none p-4 w-full bg-inherit text-inherit placeholder:text-gray-400"
         type="text"
         placeholder="Search by country name or capital..."
-        onChange={(e) => setQuery(e.target.value.toLowerCase())}
+        value={input}
+        onChange={(e) => setInput(e.target.value.toLowerCase())}
       />
     </div>
   );
